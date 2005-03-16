c7a36d41076a91b4d28a73089c3644f836f62857
Andres Kievsky <ank@ank.com.ar>
2005-03-16 22:00:38 +0000

    Import of ANK/Catalyst-View-Mason-0.03 from CPAN.
    gitpan-cpan-distribution: Catalyst-View-Mason
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         ANK/Catalyst/Catalyst-View-Mason-0.03.tar.gz
    gitpan-cpan-author:       ANK
    gitpan-cpan-maturity:     released

---
+++ lib/Catalyst/Helper/View/Mason.pm
@@ -0,0 +1,82 @@
+package Catalyst::Helper::View::Mason;
+
+use strict;
+
+our $VERSION = '0.03';
+
+=head1 NAME
+
+Catalyst::Helper::View::Mason - Helper for Mason Views
+
+=head1 SYNOPSIS
+
+    bin/create view Mason Mason
+
+=head1 DESCRIPTION
+
+Helper for Mason Views.
+
+=head2 METHODS
+
+=head3 mk_compclass
+
+=cut
+
+sub mk_compclass {
+    my ( $self, $helper ) = @_;
+    my $file  = $helper->{file};
+    my $class = $helper->{class};
+    $helper->mk_file( $file, <<"EOF");
+package $class;
+
+use strict;
+use base 'Catalyst::View::Mason';
+
+=head1 NAME
+
+$class - Mason View Component
+
+=head1 SYNOPSIS
+
+    Very simple to use
+
+=head1 DESCRIPTION
+
+Very nice component.
+
+=head1 AUTHOR
+
+Clever guy
+
+=head1 LICENSE
+
+This library is free software . You can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
+EOF
+}
+
+=head1 SEE ALSO
+
+L<Catalyst::Manual>, L<Catalyst::Test>, L<Catalyst::Request>,
+L<Catalyst::Response>, L<Catalyst::Helper>
+
+=head1 AUTHOR
+
+Andres Kievsky, C<ank@cpan.org>
+
+based on the TT View Helper from:
+
+Sebastian Riedel, C<sri@oook.de>
+
+=head1 LICENSE
+
+This library is free software . You can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
---
+++ test.pl
@@ -0,0 +1,6 @@
+use strict;
+use Test::More tests => 2;
+
+BEGIN { use_ok('Catalyst::View::Mason') }
+BEGIN { use_ok('Catalyst::Helper::View::Mason') }
+
